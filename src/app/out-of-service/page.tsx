264e909c2fb790203072b8ad3f4c478649a04c04
Jesus Castaneda <jcs.98126@gmail.com>
2024-02-27 20:27:19 -0500

    Deleted polling and added out of service env variable

---
+++ src/app/out-of-service/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { FC } from "react";
+
+const OutOfService: FC = () => {
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl text-gray-500">Servicio fuera de linea</h1>
+    </main>
+  );
+};
+
+export default OutOfService;
